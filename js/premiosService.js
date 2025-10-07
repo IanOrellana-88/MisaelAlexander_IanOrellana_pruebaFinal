49ea2c05cbc28cbf40898a8bde6276ef038838e6
IanOrellana <20240211@ricaldone.edu.sv>
2025-10-07 14:05:14 -0600

    dasd

--- js/premiosService.js
+++ js/premiosService.js
@@ -1,33 +1,31 @@
-const API_URL = " ";
-
-
+const API_URL = "http://localhost:8080/Premios";
+ 
+ 
 export async function getPremios(){
-    const res = await fetch ('${API_URL}'/getDataPremios');
+    const res = await fetch (`${API_URL}/Mostrar`);
         return res.json();
-
+ 
 }
-
-
+ 
+ 
 export async function createPremio (data) {
-    await fetch ('${API_URL'}'/newPremio;
+    await fetch (`${API_URL}/Mostrar`, {
         method: "POST",
-        headers: { "Content-Type"; application/json"},
-            body: JSON.stringify (data)
+        headers: { "Content-Type":"application/json"},
+        body: JSON.stringify (data)
 });
 }
-
-export async function updatePremio (Id, data){
-    await fetch ('${API_URL}/updatePremio/${id}',{
+ 
+export async function updatePremio (id, data){
+    await fetch (`${API_URL}/Actualizar/${id}`,{
         method: "PUT",
         headers: {"Content-Type":"applicaction/json"},
         body: JSON.stringify (data)
     });
 }
-
+ 
 export async function  deletePremio (id) {
-    await fetch ('$API_URL')/deletePremio/${id}'{
+    await fetch (`${API_URL}/Eliminar/{id}`,{
     method: "DELETE"
   });
 }
-
-
